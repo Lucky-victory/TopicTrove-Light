a9f50c1d65939c64f266edb03896cd58a27bb2f9
degencodebeast <degencodebeast@protonmail.com>
2024-01-31 13:39:26 +0100

    added social login with web3 auth

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,38 +1,75 @@
 import MainChakraProvider from "@/providers/chakra";
 import "@/styles/globals.css";
-import 'material-symbols/outlined.css'
+import "material-symbols/outlined.css";
 import type { AppProps } from "next/app";
-import { WagmiConfig, createConfig } from "wagmi";
 import {
-    ConnectKitProvider,
-    ConnectKitButton,
-    getDefaultConfig,
+  ConnectButton,
+  connectorsForWallets,
+  RainbowKitProvider,
+  darkTheme,
+} from "@rainbow-me/rainbowkit";
+import { WagmiConfig, createConfig, configureChains } from "wagmi";
+import {
+  ConnectKitProvider,
+  ConnectKitButton,
+  getDefaultConfig,
 } from "connectkit";
+import "@rainbow-me/rainbowkit/styles.css";
+import { rainbowWeb3AuthConnector } from "../components/RainbowWeb3AuthConnector";
+import { mainnet, polygon, optimism, arbitrum, base, zora } from "wagmi/chains";
+import { rainbowWallet, metaMaskWallet } from "@rainbow-me/rainbowkit/wallets";
+import { publicProvider } from "wagmi/providers/public";
 
-const config = createConfig(
-    getDefaultConfig({
-        // Required API Keys
-        alchemyId: process.env.ALCHEMY_ID, // or infuraId
-        walletConnectProjectId: process.env.WALLETCONNECT_PROJECT_ID as string,
-
-        // Required
-        appName: "Your App Name",
-
-        // Optional
-        appDescription: "Your App Description",
-        appUrl: "https://family.co", // your app's url
-        appIcon: "https://family.co/logo.png", // your app's icon, no bigger than 1024x1024px (max. 1MB)
-    })
+const { chains, publicClient } = configureChains(
+  [mainnet, polygon, optimism, arbitrum, base, zora],
+  [publicProvider()],
 );
 
+const config = createConfig(
+  getDefaultConfig({
+    // Required API Keys
+    alchemyId: process.env.ALCHEMY_ID, // or infuraId
+    walletConnectProjectId: process.env.WALLETCONNECT_PROJECT_ID as string,
+
+    // Required
+    appName: "Your App Name",
+
+    // Optional
+    appDescription: "Your App Description",
+    appUrl: "https://family.co", // your app's url
+    appIcon: "https://family.co/logo.png", // your app's icon, no bigger than 1024x1024px (max. 1MB)
+  }),
+);
+
+const connectors = connectorsForWallets([
+  {
+    groupName: "Recommended",
+    wallets: [
+      rainbowWallet({ projectId: "04309ed1007e77d1f119b85205bb779d", chains }),
+      rainbowWeb3AuthConnector({ chains }),
+      metaMaskWallet({ projectId: "04309ed1007e77d1f119b85205bb779d", chains }),
+    ],
+  },
+]);
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient,
+});
+
 export default function App({ Component, pageProps }: AppProps) {
-    return (
-        <WagmiConfig config={config}>
-            <ConnectKitProvider>
-                <MainChakraProvider>
-                    <Component {...pageProps} />
-                </MainChakraProvider>
-            </ConnectKitProvider>
-        </WagmiConfig>
-    );
+  return (
+    <WagmiConfig config={wagmiConfig}>
+      <RainbowKitProvider
+        chains={chains}
+        theme={darkTheme()}
+        modalSize="compact"
+      >
+        <MainChakraProvider>
+          <Component {...pageProps} />
+        </MainChakraProvider>
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
 }
